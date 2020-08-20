c1614836e4f3f6f40dbe01851f1a30a6badf6e17
Nick <neverate@gmail.com>
2020-08-20 13:41:01 +0300

    stage1: +basic command loop in main menu.

--- src/server/UserInterface.java
+++ src/server/UserInterface.java
@@ -1,20 +1,60 @@
 package server;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 class UserInterface {
     private final Scanner sc = new Scanner(System.in);
+    private FileServer server;
+
     void start() {
+        server = new FileServer();
         runMainMenuLoop();
     }
 
     private void runMainMenuLoop() {
+        final String cmdExit = "exit";
+        final String cmdAdd = "add";
+        final String cmdGet = "get";
+        final String cmdDel = "delete";
+        String[] commands = {cmdAdd, cmdGet, cmdDel};
+
         while (true) {
             String userInput = sc.nextLine().toLowerCase();
-            if ("exit".equals(userInput)) {
-                break;
+            if (cmdExit.equals(userInput)) {
+                return;
             }
-            System.out.println("Doing stuff!");
+            String[] inputTokens = userInput.split("\\s+");
+            if (inputTokens.length != 2) {
+                System.out.println("Error! Invalid format. Expected: command argument");
+                continue;
+            }
+            String cmd = inputTokens[0];
+            String fileName = inputTokens[1];
+            String response;
+            switch (cmd) {
+                case cmdAdd:
+                    response = server.add(fileName)
+                        ? "The file " + fileName + " added successfully"
+                        : "Cannot add the file " + fileName;
+                    break;
+                case cmdGet:
+                    AbstractFile result = server.get(fileName);
+                    response = result != null
+                        ? "The file " + fileName + " was sent"
+                        : "The file " + fileName + " not found";
+                    break;
+                case cmdDel:
+                    response = server.delete(fileName)
+                        ? "The file " + fileName + " was deleted"
+                        : "The file " + fileName + " not found";
+                    break;
+                default:
+                    response = "Error! Unrecognized command. Expected one of: "
+                        + Arrays.toString(commands);
+                    break;
+            }
+            System.out.println(response);
         }
     }
 }
