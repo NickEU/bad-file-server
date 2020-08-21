2b161b828cc983746932162a70b999f7851b8080
Nick <neverate@gmail.com>
2020-08-21 09:24:40 +0300

    stage1: implemented spec for stage1

--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -1,20 +1,27 @@
 package server;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 class FileServer {
-    List<AbstractFile> files = new ArrayList<>();
+    Map<String, AbstractFile> files = new HashMap<>();
 
     public boolean add(String fileName) {
-        return true;
+        if (fileName.matches("^file(\\d|10)$")) {
+            if (files.get(fileName) != null) {
+                return false;
+            }
+            files.put(fileName, new AbstractFile(fileName));
+            return true;
+        }
+        return false;
     }
 
     public AbstractFile get(String fileName) {
-        return null;
+        return files.get(fileName);
     }
 
     public boolean delete(String fileName) {
-        return true;
+        return files.remove(fileName) != null;
     }
 }
