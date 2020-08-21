7e5e304854f21ca548f3609ea9b5e6d8029a14c1
Nick <neverate@gmail.com>
2020-08-21 10:42:09 +0300

    stage2: Client: basic functionality

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,29 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
 class Client {
+    final String SERVER_ADDRESS = "127.0.0.1";
+    final int PORT = 33333;
+
     void start() {
         System.out.println("Client started!");
+        try {
+            InetAddress ip = InetAddress.getByName(SERVER_ADDRESS);
+            Socket socket = new Socket(ip, PORT);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            String msg = "Give me everything you have!";
+            output.writeUTF(msg);
+            System.out.println("Sent: " + msg);
+            String response = input.readUTF();
+            System.out.println("Received: " + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
