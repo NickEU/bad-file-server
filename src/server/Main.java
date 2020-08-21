2c38e911b8d6d5ab7797b269eb5d832783562756
Nick <neverate@gmail.com>
2020-08-21 10:41:55 +0300

    stage2: setting up for stage UI - small tweaks

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,7 +2,6 @@ package server;
 
 public class Main {
     public static void main(String[] args) {
-        var ui = new UserInterface();
-        ui.start();
+        new UserInterface().start();
     }
 }
