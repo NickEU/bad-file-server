19dec2cf7cada357ca4492c3c117b5a9827cc8e2
Nick <neverate@gmail.com>
2020-08-22 18:24:31 +0300

    stage2: FileServer:basic server-client interaction

--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -1,5 +1,12 @@
 package server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 class FileServer {
     final String SERVER_IP_ADDRESS = "127.0.0.1";
     final int PORT = 33333;
@@ -7,7 +14,20 @@ class FileServer {
 
     void start() {
         System.out.println("Server started!");
-        // do your server stuff
+        try {
+            InetAddress ip = InetAddress.getByName(SERVER_IP_ADDRESS);
+            ServerSocket server = new ServerSocket(PORT, 99, ip);
+            Socket socket = server.accept();
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+            String req = input.readUTF();
+            System.out.println("Received: " + req);
+            String res = "All files were sent!";
+            output.writeUTF(res);
+            System.out.println("Sent: " + res);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean add(String fileName) {
