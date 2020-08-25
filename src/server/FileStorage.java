56a873661f51f52f6faa9fcb6516aea23f182fa2
Nick <neverate@gmail.com>
2020-08-25 16:51:45 +0300

    stage4: FileStorage: map is now persistent. ID generation is now consistent.

--- src/server/FileStorage.java
+++ src/server/FileStorage.java
@@ -12,39 +12,66 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 class FileStorage {
-    private static int id = 0;
+    private static int id;
     private final Map<String, String> idsToNames = new HashMap<>();
-    private final String pathToDir = "src" + File.separator + "server"
+    private final String pathToDataDir = "src" + File.separator + "server"
         + File.separator + "data" + File.separator;
+    private final String pathToConfigFile = "src" + File.separator + "server"
+        + File.separator + "config.txt";
+    private final String SCARY_DELIMITER = "===";
 
     public FileStorage() {
         try {
-            Files.createDirectories(Paths.get(pathToDir));
+            Files.createDirectories(Paths.get(pathToDataDir));
+            readMapFromFile();
+            id = idsToNames.size();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     String add(String fileName, String fileContent) {
-        File file = new File(pathToDir + fileName);
+        File file = new File(pathToDataDir + fileName);
         if (file.exists()) {
             return null;
         }
 
         try (PrintWriter pw = new PrintWriter(file)) {
             pw.print(fileContent);
-            idsToNames.put(String.valueOf(id), fileName);
-            return String.valueOf(id++);
+            return saveIdToMap(fileName);
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
 
+    private String saveIdToMap(String fileName) {
+        idsToNames.put(String.valueOf(id), fileName);
+        saveMapToFile();
+        return String.valueOf(id++);
+    }
+
+    private void saveMapToFile() {
+        try (PrintWriter pw = new PrintWriter(pathToConfigFile)) {
+            idsToNames.forEach((k, v) -> pw.println(k + SCARY_DELIMITER + v));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void readMapFromFile() {
+        try {
+            Files.lines(Paths.get(pathToConfigFile))
+                .map(s -> s.split(SCARY_DELIMITER))
+                .forEach(s -> idsToNames.put(s[0], s[1]));
+        } catch (IOException ignored) {
+        }
+    }
+
     AbstractFile get(String identifier, boolean isId) {
         try {
             String fileName = getFilenameFromId(identifier, isId);
-            String content = Files.lines(Paths.get(pathToDir + fileName))
+            String content = Files.lines(Paths.get(pathToDataDir + fileName))
                 .collect(Collectors.joining());
             return new AbstractFile(identifier, content);
         } catch (IOException e) {
@@ -55,7 +82,7 @@ class FileStorage {
     boolean delete(String fileName) {
         try {
             // TODO : make it support IDs
-            Path p = Paths.get(pathToDir + fileName);
+            Path p = Paths.get(pathToDataDir + fileName);
             return Files.deleteIfExists(p);
         } catch (IOException e) {
             return false;
