68981ac94030c1ec4504c78200b5e1459c7bd793
Nick <neverate@gmail.com>
2020-08-27 17:43:28 +0300

    stage4: FileServer:huge refactoring of processReq: - refactored put() - implemented get() to work with byte[]

--- src/server/FileServer.java
+++ src/server/FileServer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Random;
 
 class FileServer {
@@ -30,8 +31,7 @@ class FileServer {
                     if ("exit".equalsIgnoreCase(req.trim())) {
                         return;
                     }
-                    String response = processRequest(req.split(API.COMMAND_ARG_SEPARATOR), input);
-                    output.writeUTF(response);
+                    processRequest(req.split(API.COMMAND_ARG_SEPARATOR), input, output);
                 }
             }
         } catch (IOException e) {
@@ -39,37 +39,64 @@ class FileServer {
         }
     }
 
-    private String processRequest(String[] req, DataInputStream input) {
+    private void processRequest(String[] req, DataInputStream input, DataOutputStream output) throws IOException {
         String reqType = req[0];
+        String response;
         switch (reqType) {
             case API.HTTP_REQUEST_METHOD_GET:
-                AbstractFile result = get(req[2], API.REQ_FILE_BY_ID.equals(req[1]));
-                return result != null
-                    ? API.STATUS_CODE_200 + API.COMMAND_ARG_SEPARATOR + result
-                    : API.STATUS_CODE_404;
-            case API.HTTP_REQUEST_METHOD_PUT:
-                String desiredFileName = req.length == 1 ? "" : req[1];
-                String fileName = desiredFileName.isEmpty()
-                    ? generateFileName()
-                    : desiredFileName;
                 try {
-                    int fileContentLength = input.readInt();
-                    byte[] fileContent = new byte[fileContentLength];
-                    input.readFully(fileContent, 0, fileContentLength);
-                    String id = add(fileName, fileContent);
-                    return id != null
-                        ? API.STATUS_CODE_200 + API.COMMAND_ARG_SEPARATOR + id
-                        : API.STATUS_CODE_403;
+                    processGetRequest(req, output);
                 } catch (IOException e) {
-                    return API.STATUS_CODE_404;
+                    output.writeUTF(API.STATUS_CODE_404);
                 }
+                return;
+            case API.HTTP_REQUEST_METHOD_PUT:
+                try {
+                    output.writeUTF(processPutRequest(req, input));
+                } catch (IOException e) {
+                    output.writeUTF(API.STATUS_CODE_403);
+                }
+                return;
             case API.HTTP_REQUEST_METHOD_DELETE:
-                return delete(req[2], API.REQ_FILE_BY_ID.equals(req[1]))
+                response = delete(req[2], API.REQ_FILE_BY_ID.equals(req[1]))
                     ? API.STATUS_CODE_200
                     : API.STATUS_CODE_404;
+                break;
             default:
-                return API.STATUS_CODE_404;
+                response = API.STATUS_CODE_404;
+                break;
         }
+        try {
+            output.writeUTF(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String processPutRequest(String[] req, DataInputStream input) throws IOException {
+        String desiredFileName = req.length == 1 ? "" : req[1];
+        String fileName = desiredFileName.isEmpty()
+            ? generateFileName()
+            : desiredFileName;
+
+        int fileContentLength = input.readInt();
+        byte[] fileContent = new byte[fileContentLength];
+        input.readFully(fileContent, 0, fileContentLength);
+        String id = add(fileName, fileContent);
+        return id != null
+            ? API.STATUS_CODE_200 + API.COMMAND_ARG_SEPARATOR + id
+            : API.STATUS_CODE_403;
+    }
+
+    private void processGetRequest(String[] req, DataOutputStream output) throws IOException {
+        AbstractFile result = get(req[2], API.REQ_FILE_BY_ID.equals(req[1]));
+        if (result == null) {
+            throw new IOException();
+        }
+        output.writeUTF(API.STATUS_CODE_200);
+        byte[] fileContents = Objects.requireNonNull(result).getContents();
+        output.writeInt(fileContents.length);
+        output.write(fileContents);
     }
 
     private String generateFileName() {
